c910b1ca62f63aacaaa5cd26c689d6beb41578d5
Chi Duong <chiduong1412@gmail.com>
2020-09-24 17:44:49 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,27 +16,26 @@ function ternaryTeenager(age) {
 return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
-function switchAge(age)
-switch(age) {
-case "13":
+function switchAge(age){
+case 13:
 console.log("You are a teenager");
 break;
-case "14":
+case 14:
 console.log("You are a teenager");
 break;
-case "15":
+case 15:
 console.log("You are a teenager");
 break;
-case "16":
+case 16:
 console.log("You are a teenager");
 break;
-case "17":
+case 17:
 console.log("You are a teenager");
 break;
-case "18":
+case 18:
 console.log("You are a teenager");
 break;
-case "19":
+case 19:
 console.log("You are a teenager");
 break;
 default:
